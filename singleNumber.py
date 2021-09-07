790b85d7c712e2a05c102fa192e5172dea6cbd19
Leah Ewing <leahewing@live.com>
2021-09-07 02:46:25 +0000

    improvements singleNumber.py

--- singleNumber.py
+++ singleNumber.py
@@ -24,22 +24,17 @@ You must implement a solution with a linear runtime complexity and use only cons
 
 def singleNumber(nums):
 
-    multiples = []
-    answer = None
+    nums_set = set()
     
-    for x in range(len(nums)-1):
-        y = x + 1
-        
-        for n in range(y, len(nums)):
-            if nums[x] == nums[n] and nums[x] not in multiples:
-                multiples.append(nums[x])
-
     for num in nums:
-        if num not in multiples:
-            answer = num
+        if num in nums_set:
+            nums_set.remove(num)
+        else:
+            nums_set.add(num)
+
+    answer = list(nums_set)[0]
 
     print(answer)
-
 
 
 if __name__ == '__main__':
