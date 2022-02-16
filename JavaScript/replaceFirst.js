5ca2f6989e1b2bde433ed138a21dc24234e07b34
Leah Ewing <leahewing@live.com>
2022-02-16 07:56:05 +0000

    replaceFirst.js

---
+++ JavaScript/replaceFirst.js
@@ -0,0 +1,21 @@
+/*
+
+In a given list the first element should become the last one. 
+
+An empty list or list with only one element should stay the same.
+
+Input: Array
+Output: Array
+
+*/
+
+function replaceFirst(items) {
+    if (items.length > 0) {
+        items.push(items[0]);
+        items.shift();
+    } console.log(items);
+};
+
+replaceFirst([1, 2, 3, 4]); // [2, 3, 4, 1]
+replaceFirst([1]); // [1]
+replaceFirst([]); // []
