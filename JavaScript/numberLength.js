c63627615c8a4df8a9f96437e98af5af87dc09b9
Leah Ewing <leahewing@live.com>
2022-02-16 06:46:20 +0000

    numberLength.js

---
+++ JavaScript/numberLength.js
@@ -0,0 +1,18 @@
+/*
+
+Given a positive integer, return how many digits it has.
+
+Input: A positive Int
+Output: An Int
+
+*/
+
+function numberLength(a) {
+    console.log(String(a).length);
+};
+
+
+numberLength(10); // 2
+numberLength(0); // 1
+numberLength(4); // 1
+numberLength(44); // 2
