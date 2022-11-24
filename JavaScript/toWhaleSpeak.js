14e2b489eb6e380a4c1bd59e6e58457ec0fc8b6c
Leah Ewing <leahewing@Leahs-Air.attlocal.net>
2022-11-24 13:08:36 -0600

    toWhaleSpeak.js

---
+++ JavaScript/toWhaleSpeak.js
@@ -0,0 +1,31 @@
+/*
+
+Create a function that converts a string to "Whale Speak"
+
+Rules for "Whale Speak":
+- no consanents only vowels
+- 'u's and 'e's are doubled
+
+*/
+
+
+function toWhaleSpeak(input = "This is whale speak!") {
+    const vowels = ["a", "e", "i", "o", "u"]
+    let resultsArray = []
+
+    for (let i = 0; i < input.length; i++) {
+        for (let j = 0; j < vowels.length; j++) {
+            if (input[i] == vowels[j]) {
+                    if (input[i] == "e" || input[i] == "u") {
+                        resultsArray.push(input[i])
+                }
+                resultsArray.push(input[i])
+            }
+        }
+    } 
+    const resultString = resultsArray.join('').toUpperCase()
+    console.log(resultString)
+}
+
+toWhaleSpeak("Turpentine And Turtles") // UUEEIEEUUEE
+toWhaleSpeak() // IIAEEEEA
