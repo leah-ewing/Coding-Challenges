b8ef74941bf4b600757cf08188aa656080b48ee7
Leah Ewing <leahewing@live.com>
2022-11-27 11:18:07 -0600

    allTheSame.js

---
+++ JavaScript/allTheSame.js
@@ -0,0 +1,22 @@
+/*
+
+Check if all elements in the givenarray are equal
+
+*/
+
+
+function allTheSame(elements) {
+    const reducedElements = new Set(elements)
+    
+    const same = reducedElements.size <= 1 ? true : false
+    
+    console.log(same)
+
+}
+
+allTheSame([1, 1, 1]) // true
+allTheSame([1, 2, 1]) // false
+allTheSame([1, 1, 1, 2]), //  false
+allTheSame([2, 1, 1, 1]) // false
+allTheSame([]) // true
+allTheSame([1]) // true
